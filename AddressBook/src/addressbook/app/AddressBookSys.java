8ef78a6174b23eaa3169290431b5a100946f7916
Lauryn Shields <lauryn.shields@dawsoncollege.qc.ca>
2017-08-25 12:33:21 -0400

    Create AddressBook application

---
+++ AddressBook/src/addressbook/app/AddressBookSys.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package addressbook.app;
+import lib.Address;
+
+/**
+ * @author 1634572
+ *
+ */
+public class AddressBookSys {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Address myAddress = new Address("3040", "Sherbrooke");
+		System.out.println(myAddress.getAddress());
+
+
+	}
+
+}
